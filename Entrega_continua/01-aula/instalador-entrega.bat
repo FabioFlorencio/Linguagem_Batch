74cd816eab58d082b113d45c8a0e2444ce208b56
Fabio Florencio <fabioflorencioxyz@gmail.com>
2023-03-08 11:28:10 -0300

    novos arquivos

---
+++ Entrega_continua/01-aula/instalador-entrega.bat
@@ -0,0 +1,19 @@
+@ echo off
+
+
+:: Muda para o caminho especificado
+cd\Users\DTI\Documents\
+
+:: Cria pasta
+md ALP_Labs
+
+:: Abri programa e arquivo
+start "" "C:\Users\dti\visualg30.exe" "C:\Users\dti\LAB1oficial.ALG"
+
+time /t 10
+
+pause
+
+start "" "C:\Users\dti\visualg30.exe" "C:\Users\dti\LAB.ALG"
+
+pause
